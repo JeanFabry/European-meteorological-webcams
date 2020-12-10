676a2e089526786bfd2f6af49522923d8c91d86f
JeanFabry <jeanfabry1435@gmail.com>
2020-12-10 21:20:05 +0100

    checking if webcam works

--- script.js
+++ script.js
@@ -1,52 +1,37 @@
-let webcams = 
-[
-    {
-        mainPlace: "Les Contamines-Montjoie",
-        subPlaces:[
-            {
-                subPlace: "Montagne 1",
-                link: '<a name="windy-webcam-timelapse-player" data-id="1208634909" data-play="day" href="https://windy.com/webcams/1208634909" target="_blank">Les Contamines-Montjoie</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>'
-            },
-            {
-                subPlace: "place du village",
-                link: '<a name="windy-webcam-timelapse-player" data-id="1208634909" data-play="day" href="https://windy.com/webcams/1208634909" target="_blank">Les Contamines-Montjoie</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>'
-            },
-            {
-                subPlace: "Mont Blanc",
-                link: '<a name="windy-webcam-timelapse-player" data-id="1462285137" data-play="day" href="https://windy.com/webcams/1462285137" target="_blank">Saint-Gervais-les-Bains: Mont Blanc</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>'
-            }
-        ]
-    },
-    {
-        place: "Saint-Gervais - Mont Joux",
-        link: "this",
-    },
-    {
-        place: "here",
-        link: "this",
-    },
-    {
-        place: "here",
-        link: "this",
-    },
-    {
-        place: "here",
-        link: "this",
-    },
-    {
-        place: "here",
-        link: "this",
-    },
-    {
-        place: "here",
-        link: "this",
-    },
-    {
-        place: "here",
-        link: "this",
-    },
-    {
-        place: "here",
-        link: "this",
+let section =document.querySelector('.columns');
+
+const displayWebcam =()=>{
+
+}
+
+for (let mainplace of webcams){
+    const webcamsDiv = document.createElement("div");
+    webcamsDiv.classList.add("column");
+    webcamsDiv.classList.add("is-one-quarter");
+
+    const webcamsDivTitle = document.createElement("h2");
+    webcamsDivTitle.textContent=mainplace.mainPlace;
+
+    const webcamSelect = document.createElement("select")
+     const chooseLocation = document.createElement("option");
+      chooseLocation.textContent="Choisir Localisation";
+      webcamSelect.appendChild(chooseLocation);
+    
+const webcamDiv = document.createElement("div");
+webcamDiv.classList.add("webcamDiv")
+console.log(mainplace.subPlaces[0].link);
+webcamDiv.innerHTML=mainplace.subPlaces[0].link
+
+    webcamsDiv.appendChild(webcamsDivTitle);
+    webcamsDiv.appendChild(webcamSelect);
+    webcamsDiv.appendChild(webcamDiv);
+    section.appendChild(webcamsDiv)
+    for (let subplace of mainplace.subPlaces){
+      const webcamChoice = document.createElement("option");
+      webcamChoice.textContent=subplace.subPlace;
+      webcamSelect.appendChild(webcamChoice);
+
+      webcamChoice.addEventListener("click", displayWebcam)
     }
-];
+    
+}
