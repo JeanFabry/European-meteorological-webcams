7ac3c8c0b5646da10b3f5dc4e08f0364ea04dc1c
JeanFabry <jeanfabry1435@gmail.com>
2020-12-18 22:28:00 +0100

    API is working and Data cities there

--- data.js
+++ data.js
@@ -1,97 +1,99 @@
-let webcams = [
-  {
-    mainPlace: "Les Contamines-Montjoie",
-    subPlaces: [
-      {
-        subPlace: "Les Contamines-Montjoie - Montagnes",
-        link:
-          'https://webcams.windy.com/webcams/public/embed/script/player.js',
-      },
-      {
-        subPlace: "Evasion - Mont-Banc",
-        link:
-          '<a name="windy-webcam-timelapse-player" data-id="1462285167" data-play="day" href="https://windy.com/webcams/1462285167" target="_blank">Megève: Evasion - Mont Blanc</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
-      },
-      {
-        subPlace: "Saint-Gervais-les-Bains: Mont Blanc",
-        link:
-          'https://webcams.windy.com/webcams/public/embed/script/player.js',
-      },
-      {
-        subPlace: "Saint-Gervais - Mont Joux",
-        link:
-          '<a name="windy-webcam-timelapse-player" data-id="1605270647" data-play="day" href="https://windy.com/webcams/1605270647" target="_blank">Saint-Gervais-les-Bains: Webcam de Saint-Gervais - Mont Joux</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
-      },
-    ],
-  },
-  {
-    mainPlace: "Val-d'Isère",
-    subPlaces: [
-      {
-        subPlace: "Val-d'Isère",
-        link:
-          '<a name="windy-webcam-timelapse-player" data-id="1584153682" data-play="day" href="https://windy.com/webcams/1584153682" target="_blank">Val-d\'Isère</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
-      },
-    ],
-  },
-  {
-    mainPlace: "Bruxelles",
-    subPlaces: [
-      {
-        subPlace: "Bruxelles",
-        link:
-          '<a name="windy-webcam-timelapse-player" data-id="1580391487" data-play="day" href="https://windy.com/webcams/1580391487" target="_blank">Ville de Bruxelles: Webcam at the Grand-mainPlace of Brussels</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
-      },
-    ],
-  },
-  {
-    mainPlace: "Anvers",
-    subPlaces: [
-      {
-        subPlace: "Anvers",
-        link:
-          '<a name="windy-webcam-timelapse-player" data-id="1296578281" data-play="day" href="https://windy.com/webcams/1296578281" target="_blank">Anvers › Nord: Zicht op de schelde</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
-      },
-    ],
-  },
-  {
-    mainPlace: "Paris",
-    subPlaces: [
-      {
-        subPlace: "Paris",
-        link:
-          '<a name="windy-webcam-timelapse-player" data-id="1549489174" data-play="day" href="https://windy.com/webcams/1549489174" target="_blank">Paris: Arc de Triomphe - Eiffel Tower</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
-      },
-    ],
-  },
-  {
-    mainPlace: "Berlin",
-    subPlaces: [
-      {
-        subPlace: "Berlin",
-        link:
-          '<a name="windy-webcam-timelapse-player" data-id="1230040329" data-play="day" href="https://windy.com/webcams/1230040329" target="_blank">Berlin: Town Hall - Rathaus</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
-      },
-    ],
-  },
-  {
-    mainPlace: "Amsterdam",
-    subPlaces: [
-      {
-        subPlace: "Amsterdam",
-        link:
-          '<a name="windy-webcam-timelapse-player" data-id="1511301958" data-play="day" href="https://windy.com/webcams/1511301958" target="_blank">Amsterdam: W</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
-      },
-    ],
-  },
-  {
-    mainPlace: "Londres",
-    subPlaces: [
-      {
-        subPlace: "Londres",
-        link:
-          '<a name="windy-webcam-timelapse-player" data-id="1508414460" data-play="day" href="https://windy.com/webcams/1508414460" target="_blank">Londres: Oxford Circus</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
-      },
-    ],
-  },
-];
+// let webcams = [
+//   {
+//     mainPlace: "Les Contamines-Montjoie",
+//     subPlaces: [
+//       {
+//         subPlace: "Les Contamines-Montjoie - Montagnes",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1208634909" data-play="day" href="https://windy.com/webcams/1208634909" target="_blank">Les Contamines-Montjoie</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//       {
+//         subPlace: "Evasion - Mont-Banc",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1462285167" data-play="day" href="https://windy.com/webcams/1462285167" target="_blank">Megève: Evasion - Mont Blanc</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//       {
+//         subPlace: "Saint-Gervais-les-Bains: Mont Blanc",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1462285137" data-play="day" href="https://windy.com/webcams/1462285137" target="_blank">Saint-Gervais-les-Bains: Mont Blanc</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//       {
+//         subPlace: "Saint-Gervais - Mont Joux",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1605270647" data-play="day" href="https://windy.com/webcams/1605270647" target="_blank">Saint-Gervais-les-Bains: Webcam de Saint-Gervais - Mont Joux</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//     ],
+//   },
+//   {
+//     mainPlace: "Val-d'Isère",
+//     subPlaces: [
+//       {
+//         subPlace: "Val-d'Isère",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1584153682" data-play="day" href="https://windy.com/webcams/1584153682" target="_blank">Val-d\'Isère</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//     ],
+//   },
+//   {
+//     mainPlace: "Bruxelles",
+//     subPlaces: [
+//       {
+//         subPlace: "Bruxelles",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1580391487" data-play="day" href="https://windy.com/webcams/1580391487" target="_blank">Ville de Bruxelles: Webcam at the Grand-mainPlace of Brussels</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//     ],
+//   },
+//   {
+//     mainPlace: "Anvers",
+//     subPlaces: [
+//       {
+//         subPlace: "Anvers",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1296578281" data-play="day" href="https://windy.com/webcams/1296578281" target="_blank">Anvers › Nord: Zicht op de schelde</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//     ],
+//   },
+//   {
+//     mainPlace: "Paris",
+//     subPlaces: [
+//       {
+//         subPlace: "Paris",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1549489174" data-play="day" href="https://windy.com/webcams/1549489174" target="_blank">Paris: Arc de Triomphe - Eiffel Tower</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//     ],
+//   },
+//   {
+//     mainPlace: "Berlin",
+//     subPlaces: [
+//       {
+//         subPlace: "Berlin",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1230040329" data-play="day" href="https://windy.com/webcams/1230040329" target="_blank">Berlin: Town Hall - Rathaus</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//     ],
+//   },
+//   {
+//     mainPlace: "Amsterdam",
+//     subPlaces: [
+//       {
+//         subPlace: "Amsterdam",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1511301958" data-play="day" href="https://windy.com/webcams/1511301958" target="_blank">Amsterdam: W</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//     ],
+//   },
+//   {
+//     mainPlace: "Londres",
+//     subPlaces: [
+//       {
+//         subPlace: "Londres",
+//         link:
+//           '<a name="windy-webcam-timelapse-player" data-id="1508414460" data-play="day" href="https://windy.com/webcams/1508414460" target="_blank">Londres: Oxford Circus</a><script async type="text/javascript" src="https://webcams.windy.com/webcams/public/embed/script/player.js"></script>',
+//       },
+//     ],
+//   },
+// ];
+
+
--- script.js
+++ script.js
@@ -1,40 +1,69 @@
-let section =document.querySelector('.columns');
-let scripts = section.children;
-console.log('scripts:', scripts[0])
+let section = document.querySelector('.columns');
+// let scripts = section.children;
+// console.log('scripts:', scripts[2])
 
-const displayWebcam =()=>{
+// const displayWebcam =()=>{
 
-}
+// }
 
-for (let mainplace of webcams){
-    const webcamsDiv = document.createElement("div");
-    webcamsDiv.classList.add("column");
-    webcamsDiv.classList.add("is-one-quarter");
+// for (let mainplace of webcams){
+//     const webcamsDiv = document.createElement("div");
+//     webcamsDiv.classList.add("column");
+//     webcamsDiv.classList.add("is-one-quarter");
 
-    const webcamsDivTitle = document.createElement("h2");
-    webcamsDivTitle.textContent=mainplace.mainPlace;
+//     const webcamsDivTitle = document.createElement("h2");
+//     webcamsDivTitle.textContent=mainplace.mainPlace;
 
-    const webcamSelect = document.createElement("select")
-     const chooseLocation = document.createElement("option");
-      chooseLocation.textContent="Choisir Localisation";
-      webcamSelect.appendChild(chooseLocation);
-    
-const webcamDiv = document.createElement("div");
-webcamDiv.classList.add("webcamDiv")
-const script = document.createElement("script");
-script.src=mainplace.subPlaces[0].link
-webcamDiv.appendChild(script);
+//     const webcamSelect = document.createElement("select")
+//      const chooseLocation = document.createElement("option");
+//       chooseLocation.textContent="Choisir Localisation";
+//       webcamSelect.appendChild(chooseLocation);
 
-    webcamsDiv.appendChild(webcamsDivTitle);
-    webcamsDiv.appendChild(webcamSelect);
-    webcamsDiv.appendChild(webcamDiv);
-    section.appendChild(webcamsDiv)
-    for (let subplace of mainplace.subPlaces){
-      const webcamChoice = document.createElement("option");
-      webcamChoice.textContent=subplace.subPlace;
-      webcamSelect.appendChild(webcamChoice);
+// const webcamDiv = document.createElement("div");
+// webcamDiv.classList.add("webcamDiv")
+// const script = document.createElement("script");
+// script.src=mainplace.subPlaces[0].link
+// webcamDiv.appendChild(script);
 
-      webcamChoice.addEventListener("click", displayWebcam)
-    }
-    
-}
+//     webcamsDiv.appendChild(webcamsDivTitle);
+//     webcamsDiv.appendChild(webcamSelect);
+//     webcamsDiv.appendChild(webcamDiv);
+//     section.appendChild(webcamsDiv)
+//     for (let subplace of mainplace.subPlaces){
+//       const webcamChoice = document.createElement("option");
+//       webcamChoice.textContent=subplace.subPlace;
+//       webcamSelect.appendChild(webcamChoice);
+
+//       webcamChoice.addEventListener("click", displayWebcam)
+//     }
+
+// }
+
+fetch(
+        //"https://api.windy.com/api/webcams/v2/list/country=FR/category=beach/orderby=popularity/limit=8?show=webcams:player&key=yQOnE7r9ViOUBnz4gaHzWOZmK751RHSm"
+        "https://api.windy.com/api/webcams/v2/list/nearby=45.814568245813724,6.722398313598164,10/?show=webcams:player&key=yQOnE7r9ViOUBnz4gaHzWOZmK751RHSm"
+    )
+    .then((response) => response.json())
+    .then((mainObject) => {
+        console.log(mainObject);
+        console.log(mainObject.result.webcams);
+        for (let webcamsObj of mainObject.result.webcams) {
+            let webcamEmbed = document.createElement("embed");
+            webcamEmbed.type = "video/webm";
+            webcamEmbed.classList.add("webcamsStyle")
+            if (webcamsObj.player.live.available) {
+                webcamEmbed.src = webcamsObj.player.live.embed
+            } else {
+                webcamEmbed.src = webcamsObj.player.day.embed
+            }
+            section.appendChild(webcamEmbed);
+            console.log('webcamEmbed:', webcamEmbed)
+            
+        }
+        console.log(mainObject.result.webcams[0].player);
+
+
+        /* <embed src="https://webcams.windy.com/webcams/public/embed/player/1301219390/day" type="video/webm"  width="250"
+               height="200"></embed> */
+
+    });
